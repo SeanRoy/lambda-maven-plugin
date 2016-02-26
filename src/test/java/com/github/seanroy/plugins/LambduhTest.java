e9a7053483528a84d5d76e1dbc23561b2a39427a
Sean Roy <sean.roy@ping4.com>
2016-02-26 11:29:22 -0500

    Lambduh test harness

---
+++ src/test/java/com/github/seanroy/plugins/LambduhTest.java
@@ -0,0 +1,33 @@
+package com.github.seanroy.plugins;
+
+import java.io.File;
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.junit.Test;
+
+public class LambduhTest extends AbstractMojoTestCase {
+
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+    }
+
+    protected void tearDown()
+        throws Exception
+    {
+        super.tearDown();
+    }
+    
+    
+    @Test
+    public void test() throws Exception {
+        File pom = getTestFile("src/test/resources/test-project/pom.xml");
+        assertNotNull( pom );
+        assertTrue( pom.exists() );
+
+        LambduhMojo myMojo = (LambduhMojo) lookupMojo( "deploy-lambda", pom );
+        assertNotNull( myMojo );
+        myMojo.execute();
+    }
+}
