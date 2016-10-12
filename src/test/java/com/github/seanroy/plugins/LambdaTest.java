a2e4cecdc3d1808f7fbab94192c9571692895b86
Sean Roy <sean.roy@ping4.com>
2016-10-12 12:17:09 -0400

    Merged trigger config into test

--- src/test/java/com/github/seanroy/plugins/LambdaTest.java
+++ src/test/java/com/github/seanroy/plugins/LambdaTest.java
@@ -1,16 +1,48 @@
 package com.github.seanroy.plugins;
 
+import static java.util.Optional.of;
+
 import java.io.File;
 
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 import org.junit.Test;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.AWSLambdaClient;
+
 public class LambdaTest extends AbstractMojoTestCase {
 
+    private AWSCredentials credentials;
+    public AWSLambdaClient lambdaClient;
+    private String accessKey = null;
+    private String secretKey = null;
+    private String regionName = "us-east-1";
+    
     protected void setUp()
         throws Exception
     {
         super.setUp();
+        
+        Regions region = Regions.fromName(regionName);
+        
+        DefaultAWSCredentialsProviderChain defaultChain = new DefaultAWSCredentialsProviderChain();
+        if (accessKey != null && secretKey != null) {
+            credentials = new BasicAWSCredentials(accessKey, secretKey);
+        } else if (defaultChain.getCredentials() != null) {
+            credentials = defaultChain.getCredentials();
+        }
+
+        if (credentials == null) {
+            throw new MojoExecutionException("AWS Credentials config error");
+        }
+        
+        lambdaClient = of(credentials)
+                .map(credentials -> new AWSLambdaClient(credentials).<AWSLambdaClient>withRegion(region))
+                .orElse(new AWSLambdaClient().withRegion(region));
     }
 
     protected void tearDown()
