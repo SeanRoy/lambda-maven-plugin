63d9d13a6f578ad69828988d2950b432b7f9d653
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-02-05 11:28:10 -0600

    Another update to prevent error proprogation
    When the lambda function does not exist

--- src/main/java/com/github/seanroy/plugins/UpdateLambdaCodeMojo.java
+++ src/main/java/com/github/seanroy/plugins/UpdateLambdaCodeMojo.java
@@ -1,7 +1,11 @@
 package com.github.seanroy.plugins;
 
+import com.amazonaws.services.lambda.model.GetFunctionRequest;
+import com.amazonaws.services.lambda.model.ResourceNotFoundException;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+
+import java.util.function.Function;
 
 /**
  * I am a update code mojo responsible to update lambda function code in AWS.
@@ -19,10 +23,21 @@ public class UpdateLambdaCodeMojo extends AbstractLambdaMojo {
             lambdaFunctions.stream().map(f -> {
                 getLog().info("---- Update function code " + f.getFunctionName() + " -----");
                 return f;
-            }).forEach(lf -> updateFunctionCode.apply(lf));
+            }).forEach(lf -> updateFunctionCodeIfExists.apply(lf));
         } catch (Exception e) {
             getLog().error("Error during processing", e);
             throw new MojoExecutionException(e.getMessage());
         }
     }
+
+    private Function<LambdaFunction, LambdaFunction> updateFunctionCodeIfExists = (LambdaFunction lambdaFunction) -> {
+        try {
+            lambdaFunction.setFunctionArn(lambdaClient.getFunction(
+                    new GetFunctionRequest().withFunctionName(lambdaFunction.getFunctionName())).getConfiguration().getFunctionArn());
+            updateFunctionCode.apply(lambdaFunction);
+        } catch (ResourceNotFoundException e) {
+            getLog().info("Lambda function not found", e);
+        }
+        return lambdaFunction;
+    };
 }
