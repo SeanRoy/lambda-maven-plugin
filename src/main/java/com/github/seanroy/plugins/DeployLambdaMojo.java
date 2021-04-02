9afde508b9b3da2aa8dce0360d8aa31aaadcb9ab
John Dimeo <dimeo@elderresearch.com>
2021-04-01 20:28:37 -0400

    Automatically force an update if the version contains SNAPSHOT

--- src/main/java/com/github/seanroy/plugins/DeployLambdaMojo.java
+++ src/main/java/com/github/seanroy/plugins/DeployLambdaMojo.java
@@ -16,6 +16,8 @@ import java.util.function.BiPredicate;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 
@@ -110,7 +112,7 @@ public class DeployLambdaMojo extends AbstractLambdaMojo {
         if (!isConfigurationChanged) {
             getLog().info("Config hasn't changed for " + lambdaFunction.getFunctionName());
         }
-        if (forceUpdate) {
+        if (ObjectUtils.defaultIfNull(forceUpdate, StringUtils.containsIgnoreCase(version, "SNAPSHOT"))) {
             getLog().info("Forcing update for " + lambdaFunction.getFunctionName());
         }
 
